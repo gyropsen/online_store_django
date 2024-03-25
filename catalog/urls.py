01843ccf69e29b2717a91774fb488584c1d651da
egor <sharapov.egor00@gmail.com>
2024-03-25 23:42:08 +0300

    Homework_20.2 Done

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import index, contacts, products, write_message, catalog
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name="home"),
-    path('contacts/', contacts, name="contacts")
+    path('', index, name="index"),
+    path('contacts/', contacts, name="contacts"),
+    path('write/', write_message, name='write_message'),
+    path('catalog/', catalog, name='catalog'),
+    path('products/<int:pk>/', products, name="products"),
 ]
+# <int:pk>/
--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,49 @@
 from django.shortcuts import render
 from catalog.models import User
+from catalog.models import Product
+from catalog.models import Category
 
 
-def home(request):
-    return render(request, "catalog/home.html")
+def index(request):
+    context = {
+        'object_list': Category.objects.all(),
+        'title': 'Главная'
+    }
+    return render(request, 'catalog/index.html', context)
 
 
 def contacts(request):
+    context = {
+        'title': 'Контакты'
+    }
+
+    return render(request, 'catalog/contacts.html', context)
+
+
+def write_message(request):
     if request.method == "POST":
         name = request.POST.get("name")
         phone = request.POST.get("phone")
         message = request.POST.get("message")
         User(name=name, phone=phone, text=message).save()
-    return render(request, 'catalog/contacts.html')
+    context = {'title': 'Напишите нам'}
+
+    return render(request, 'catalog/write_mess.html', context)
+
+
+def catalog(request):
+    context = {
+        'object_list': Category.objects.all(),
+        'title': 'Каталог'
+    }
+    return render(request, 'catalog/catalog.html', context)
+
+
+def products(request, pk):
+    category_item = Category.objects.get(pk=pk)
+    context = {
+        'object_list': Product.objects.filter(category_id=pk),
+        'title': str(category_item.name),
+        'description': str(category_item.description),
+    }
+    return render(request, 'catalog/product.html', context)
