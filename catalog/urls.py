abbff20541bec93ad69245a5906be162cb5d4269
egor <sharapov.egor00@gmail.com>
2024-05-02 16:41:16 +0300

    2 task is done: cache DetailView. Fix base.html

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import IndexTemplateView, ContactTemplateView, ProductListView, WriteMessageCreateView, \
     CatalogListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
+from django.views.decorators.cache import cache_page
 
 app_name = CatalogConfig.name
 
@@ -11,7 +12,7 @@ urlpatterns = [
     path('write/', WriteMessageCreateView.as_view(), name='write_message'),
     path('catalog/', CatalogListView.as_view(), name='catalog'),
     path('products/<int:pk>/', ProductListView.as_view(), name="products"),
-    path('detail/<int:pk>/', ProductDetailView.as_view(), name="product_detail"),
+    path('detail/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name="product_detail"),
     path('create/', ProductCreateView.as_view(), name="product_create"),
     path('update/<int:pk>', ProductUpdateView.as_view(), name="product_update"),
     path('delete/<int:pk>', ProductDeleteView.as_view(), name="product_delete"),
