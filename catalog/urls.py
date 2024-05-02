b27bc7385e631bd1d1c96d654e91f10b764ec479
egor <sharapov.egor00@gmail.com>
2024-05-02 23:32:34 +0300

    additional task is done: cache middleware for site

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,18 +2,18 @@ from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import IndexTemplateView, ContactTemplateView, ProductListView, WriteMessageCreateView, \
     CatalogListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
-from django.views.decorators.cache import cache_page
+from django.views.decorators.cache import cache_page, never_cache
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', IndexTemplateView.as_view(), name="index"),
-    path('contacts/', ContactTemplateView.as_view(), name="contacts"),
-    path('write/', WriteMessageCreateView.as_view(), name='write_message'),
-    path('catalog/', CatalogListView.as_view(), name='catalog'),
-    path('products/<int:pk>/', ProductListView.as_view(), name="products"),
+    path('', never_cache(IndexTemplateView.as_view()), name="index"),
+    path('contacts/', never_cache(ContactTemplateView.as_view()), name="contacts"),
+    path('write/', cache_page(60)(WriteMessageCreateView.as_view()), name='write_message'),
+    path('catalog/', never_cache(CatalogListView.as_view()), name='catalog'),
+    path('products/<int:pk>/', never_cache(ProductListView.as_view()), name="products"),
     path('detail/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name="product_detail"),
-    path('create/', ProductCreateView.as_view(), name="product_create"),
-    path('update/<int:pk>', ProductUpdateView.as_view(), name="product_update"),
-    path('delete/<int:pk>', ProductDeleteView.as_view(), name="product_delete"),
+    path('create/', cache_page(60)(ProductCreateView.as_view()), name="product_create"),
+    path('update/<int:pk>', cache_page(60)(ProductUpdateView.as_view()), name="product_update"),
+    path('delete/<int:pk>', never_cache(ProductDeleteView.as_view()), name="product_delete"),
 ]
